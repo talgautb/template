6a0773602352e33cc425bd53b12dbbe6db4df96e
talgautb <talgautb@yandex.kz>
2015-01-08 15:59:30 +0600

    add gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,10 @@ var gulp = require('gulp'),
   uglify = require('gulp-uglify');
   // concat files
   concat = require('gulp-concat');
+  // reload browser
+  browserSync = require('browser-sync'),
+  // minify images
+  imagemin = require('gulp-imagemin');
 
 /*-------------------------------------------------------------------
   Configuration
@@ -27,22 +31,31 @@ var path = {
   stylus: "assets/stylus",
   coffee: "assets/coffee",
   jade: "assets/jade",
+  images: "assets/images",
 
   html: "dist/",
   css: "dist/css",
-  js: "dist/js"
-
-  //img: "assets/img"
+  js: "dist/js",
+  img: "dist/img"
 };
 
 var watched = {
   stylus: path.stylus + '/**/*.styl',
   coffee: path.coffee + '/**/*.coffee',
-  jade: path.jade + '/**/*.jade'
-  //img: path.img + '/**/*',
+  jade: path.jade + '/**/*.jade',
+  img: path.images + '/*'
 };
 
 /*------DEV TASKS------*/
+
+// imagemin
+gulp.task('imagemin', function() {
+  gulp.src(watched.img)
+    .pipe(imagemin({
+      progressive: true
+    }))
+    .pipe(gulp.dest(path.img));
+});
 
 // Coffee
 gulp.task('coffee', function() {
@@ -50,7 +63,10 @@ gulp.task('coffee', function() {
     .pipe(coffee({bare: true}))
     .pipe(concat('app.js'))
     .pipe(uglify({mangle: true}))
-    .pipe(gulp.dest(path.js));
+    .pipe(gulp.dest(path.js))
+    .pipe(browserSync.reload({
+      stream:true
+    }));
 });
 
 // Jade
@@ -58,7 +74,10 @@ gulp.task('jade', function() {
   gulp.src(path.jade+'/*.jade')
     .pipe(jade({pretty: true}))
     .on('error', console.log)
-    .pipe(gulp.dest(path.html));
+    .pipe(gulp.dest(path.html))
+    .pipe(browserSync.reload({
+      stream:true
+    }));
 });
 
 // Stylus
@@ -72,12 +91,23 @@ gulp.task('stylus', function() {
     .pipe(concat('index.css'))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(minifycss())
-    .pipe(gulp.dest(path.css));
+    .pipe(gulp.dest(path.css))
+    .pipe(browserSync.reload({
+      stream:true
+    }));
 });
 
+// Static server
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+        baseDir: "dist/"
+    }
+  });
+});
 
 // Watcher
-gulp.task('watch', function() {
+gulp.task('watch', ['browser-sync'], function() {
   // Create file on init
   //gulp.run('stylus');
   //gulp.run('coffee');
@@ -93,3 +123,5 @@ gulp.task('watch', function() {
   gulp.watch(watched.coffee, ['coffee']);
 
 });
+
+gulp.task('img', ['imagemin']);
